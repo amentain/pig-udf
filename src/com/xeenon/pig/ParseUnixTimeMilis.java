b7c765383a5174d1b761a577b6fb83653c4a0129
Pavel N. Savenkov <xeenon@me.com>
2013-08-07 22:03:10 +0400

    more pretty

--- src/com/xeenon/pig/ParseUnixTimeMilis.java
+++ src/com/xeenon/pig/ParseUnixTimeMilis.java
@@ -7,27 +7,25 @@ import org.apache.pig.impl.logicalLayer.schema.Schema;
 import org.apache.pig.data.DataType;
 import org.apache.pig.PigException;
 
-public class ParseUnixTimeMilis extends EvalFunc<Tuple> {
+import java.text.SimpleDateFormat;
 
-    static TupleFactory mTupleFactory = TupleFactory.getInstance();
+public class ParseUnixTimeMilis extends EvalFunc<Tuple> {
+    private static final TupleFactory mTupleFactory = TupleFactory.getInstance();
+    private static final SimpleDateFormat date = new java.text.SimpleDateFormat("yyyy-MM-dd");
+    private static final SimpleDateFormat hour = new java.text.SimpleDateFormat("H");
 
     public Tuple exec(Tuple input) throws PigException {
         if (input == null || input.size() < 1 || input.isNull(0))
             return null;
 
         try {
-
-        // Создаем Date
             java.util.Date dtDate = new java.util.Date((Long) input.get(0));
 
-        // Получаем iso-дату и номер часа
-            Tuple output = mTupleFactory.newTuple(2);
-
-                output.set(0, new java.text.SimpleDateFormat("yyyy-MM-dd").format(dtDate));
-                output.set(1, new java.text.SimpleDateFormat("H").format(dtDate));
+            Tuple output = mTupleFactory.newTuple();
+            output.append(date.format(dtDate));
+            output.append(hour.format(dtDate));
 
             return output;
-
         } catch(Exception e){
             System.err.println("Failed to process input; error - " + e.getMessage());
             throw new PigException("Exception in com.xeenon.pig.ParseUnixTimeMilis <"+ input.toString() +"> == ["+ e.getClass().toString() +"] "+ e.toString(), 1300, PigException.INPUT);
