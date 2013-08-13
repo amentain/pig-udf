1e6d250f9f54092c1fa8641a887283afcfe5dcaf
Pavel N. Savenkov <xeenon@me.com>
2013-08-13 09:17:24 +0400

    empty tests

---
+++ test/com/xeenon/pig/test/Test_IP2Long.java
@@ -0,0 +1,10 @@
+package com.xeenon.pig.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xeenon
+ * Date: 13.08.13
+ * Time: 9:16
+ */
+public class Test_IP2Long {
+}
---
+++ test/com/xeenon/pig/test/helper/RecordReader.java
@@ -0,0 +1,10 @@
+package com.xeenon.pig.test.helper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xeenon
+ * Date: 13.08.13
+ * Time: 9:15
+ */
+public class RecordReader {
+}
