16a05e663c09256d8ac329aa20a7a9363610a5fb
Pavel N. Savenkov <xeenon@me.com>
2013-08-16 09:19:24 +0400

    read array

--- test/com/xeenon/pig/test/helper/RecordReader.java
+++ test/com/xeenon/pig/test/helper/RecordReader.java
@@ -14,7 +14,13 @@ import java.nio.charset.Charset;
  * Time: 9:15
  */
 public class RecordReader extends org.apache.hadoop.mapreduce.RecordReader {
-    Boolean first = true;
+    private String[] lines;
+    private int pos = -1;
+
+    public RecordReader(String[] lines) {
+        super();
+        this.lines = lines;
+    }
 
     @Override
     public void initialize(InputSplit inputSplit, TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
@@ -22,12 +28,7 @@ public class RecordReader extends org.apache.hadoop.mapreduce.RecordReader {
 
     @Override
     public boolean nextKeyValue() throws IOException, InterruptedException {
-        if (first) {
-            first = !first;
-            return true;
-        } else {
-            return false;
-        }
+        return pos++ > lines.length;
     }
 
     @Override
@@ -37,14 +38,15 @@ public class RecordReader extends org.apache.hadoop.mapreduce.RecordReader {
 
     @Override
     public Text getCurrentValue() throws IOException, InterruptedException {
-        Text text = new Text("{\"site_id\":4334,\"page_id\":3779,\"geo_id\":0,\"uid\":\"7159008078565156977\",\"ip\":\"10.8.1.11\",\"ts\":1375869842741,\"page_url\":\"/exp?sid=4334&bt=5&bn=1&bc=3&ct=2&pr=3686&pt=b&pd=24&pw=3&pv=18&prr=\",\"page_referer\":\"\",\"time\":105,\"impressions\":[{\"id\":\"1\",\"place_id\":38454,\"type_id\":5,\"dsp_id\":0,\"frame_id\":0,\"floorPrice\":5.0,\"iprice\":0.0,\"oprice\":0.0,\"imoney\":0.0,\"omoney\":0.0,\"currency\":\"\"}],\"responses\":[],\"outsiders\":[{\"impid\":\"1\",\"dsp_id\":32,\"frame_id\":104}]}".getBytes(Charset.forName("UTF-8")));
-
-        return text;
+        return new Text(lines[pos].getBytes(Charset.forName("UTF-8")));
     }
 
     @Override
     public float getProgress() throws IOException, InterruptedException {
-        return first ? 0 : 100;
+        if (pos == 0)
+            return 0;
+
+        return lines.length / pos;
     }
 
     @Override
