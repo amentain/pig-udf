a3436e86ddf2e64d2c57ffed10f3c7cb40a2424c
Pavel N. Savenkov <xeenon@me.com>
2013-08-22 11:00:14 +0400

    test

--- test/com/xeenon/pig/test/Test_IP2Long.java
+++ test/com/xeenon/pig/test/Test_IP2Long.java
@@ -1,5 +1,13 @@
 package com.xeenon.pig.test;
 
+import com.xeenon.pig.IP2Long;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  * User: xeenon
@@ -7,9 +15,27 @@ package com.xeenon.pig.test;
  * Time: 9:16
  */
 public class Test_IP2Long {
-    public static void main(String[] args) throws Exception {
-        if (args == null || args.length < 1)
-            return;
+    public Map<String, Long> ips = new HashMap<>();
+    public TupleFactory tupleFactory = TupleFactory.getInstance();
 
+    public Test_IP2Long() {
+        ips.put("127.0.0.1", 2130706433l);
+        ips.put("194.190.116.55", 3267261495l);
+        ips.put("5.19.247.253", 85194749l);
+    }
+
+    @Test
+    public void pig() throws Exception {
+
+        IP2Long ip2Long = new IP2Long();
+        for (Map.Entry<String, Long> entry : ips.entrySet()) {
+            Tuple input = tupleFactory.newTuple(1);
+            input.set(0, entry.getKey());
+
+            Long ipn = ip2Long.exec(input);
+            if (!ipn.equals(entry.getValue())) {
+                throw new Exception("wrong ip conversion");
+            }
+        }
     }
 }
