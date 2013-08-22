5cbf1299d70fe94798da413f92044c29389716df
Pavel N. Savenkov <xeenon@me.com>
2013-08-22 09:24:32 +0400

    empty test

---
+++ test/com/xeenon/pig/test/Test_ParseUnixTimeMilis.java
@@ -0,0 +1,15 @@
+package com.xeenon.pig.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xeenon
+ * Date: 22.08.13
+ * Time: 9:24
+ */
+public class Test_ParseUnixTimeMilis {
+    public static void main(String[] args) throws Exception {
+        if (args == null || args.length < 1)
+            return;
+
+    }
+}
