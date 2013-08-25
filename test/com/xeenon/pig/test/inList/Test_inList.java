4c650b9143aaac221538dd7d1ecf7731cd1b60d9
Pavel N. Savenkov <xeenon@me.com>
2013-08-25 13:39:22 +0400

    empty test

---
+++ test/com/xeenon/pig/test/inList/Test_inList.java
@@ -0,0 +1,21 @@
+package com.xeenon.pig.test.inList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xeenon
+ * Date: 23.08.13
+ * Time: 22:27
+ */
+public class Test_inList {
+
+
+
+    public Test_inList() {
+
+
+
+    }
+
+
+
+}
