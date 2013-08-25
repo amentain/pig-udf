9539f3f39c35baf6fd7bdcb4a8e8efb18dfa2c42
Pavel N. Savenkov <xeenon@me.com>
2013-08-25 22:48:20 +0400

    simple test

--- test/com/xeenon/pig/test/inList/Test_inList.java
+++ test/com/xeenon/pig/test/inList/Test_inList.java
@@ -1,5 +1,11 @@
 package com.xeenon.pig.test.inList;
 
+import com.xeenon.pig.inList;
+import junit.framework.Assert;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+import org.junit.Test;
+
 /**
  * Created with IntelliJ IDEA.
  * User: xeenon
@@ -7,15 +13,28 @@ package com.xeenon.pig.test.inList;
  * Time: 22:27
  */
 public class Test_inList {
+    String[] test = {"one", "two", "three", "four", "five"};
+    String must = "two,five";
+    String mustNot = "six,seven";
 
+    @Test
+    public void testString() throws Exception {
+        inList filterMust = new inList(must, ",");
+        inList filterMustNot = new inList(mustNot, ",");
 
+        int mustCount = 0;
+        int mustNotCount = 0;
+        Tuple input = TupleFactory.getInstance().newTuple(1);
+        for (String str : test) {
+            input.set(0, str);
+            if (filterMust.exec(input))
+                mustCount++;
 
-    public Test_inList() {
+            if (filterMustNot.exec(input))
+                mustNotCount++;
+        }
 
-
-
+        Assert.assertEquals("positive filter", mustCount, must.split(",").length);
+        Assert.assertEquals("negative filter", mustNotCount, 0);
     }
-
-
-
 }
