7f12fd355226b576122ec593c393d6cb03252456
Pavel N. Savenkov <xeenon@me.com>
2013-08-28 10:30:59 +0400

    for future use

---
+++ src/com/xeenon/pig/IPFilter_Long.java
@@ -0,0 +1,17 @@
+package com.xeenon.pig;
+
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xeenon
+ * Date: 26.08.13
+ * Time: 15:35
+ */
+public class IPFilter_Long extends IPFilter {
+
+
+//    public IPFilter (String sFilterFile) throws IOException
+
+
+}
