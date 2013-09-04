870e7e6586f2ecd0b5612f057ecd8d7dcf25f86c
Pavel N. Savenkov <xeenon@me.com>
2013-09-04 11:57:25 +0400

    loader begings

---
+++ src/com/xeenon/pig/loader/XSchema.java
@@ -0,0 +1,30 @@
+package com.xeenon.pig.loader;
+
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+
+import java.lang.reflect.Field;
+import java.util.LinkedHashMap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xeenon
+ * Date: 03.09.13
+ * Time: 12:26
+ */
+public class XSchema extends LinkedHashMap<String, XSchemaField> {
+
+    public XSchema(Class cls) {
+        super(cls.getFields().length);
+        for (Field field : cls.getFields()) {
+            put(field.getName(), new XSchemaField(field));
+        }
+    }
+
+    public XSchema(Schema schema) {
+        super(schema.size());
+        for (Schema.FieldSchema fieldSchema : schema.getFields()) {
+            put(fieldSchema.alias, new XSchemaField(fieldSchema));
+        }
+    }
+
+}
---
+++ src/com/xeenon/pig/loader/XSchemaField.java
@@ -0,0 +1,92 @@
+package com.xeenon.pig.loader;
+
+import org.apache.pig.data.DataType;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xeenon
+ * Date: 03.09.13
+ * Time: 11:06
+ */
+public class XSchemaField {
+    private String fieldName;
+    private Class fieldClass;
+
+    private XSchema schema = null;
+
+    public XSchemaField(Field field) throws ParseException {
+        fieldName = field.getName();
+        fieldClass = field.getClass();
+
+        if (fieldClass.getFields().length > 0) {
+            schema = new XSchema(fieldClass);
+        }
+    }
+
+    public XSchemaField(Schema.FieldSchema fieldSchema) throws ParseException {
+        fieldName = fieldSchema.alias;
+        switch (fieldSchema.type) {
+            case DataType.BAG:
+            case DataType.TUPLE:
+                throw new ParseException("not implemented");
+
+            case DataType.BOOLEAN:
+                fieldClass = Boolean.class;
+                break;
+
+            case DataType.BYTE:
+                fieldClass = Byte.class;
+
+            case DataType.INTEGER:
+                fieldClass = Integer.class;
+                break;
+
+            case DataType.LONG:
+                fieldClass = Long.class;
+                break;
+
+            case DataType.FLOAT:
+                fieldClass = Float.class;
+                break;
+
+            case DataType.DOUBLE:
+                fieldClass = Double.class;
+                break;
+
+            case DataType.DATETIME:
+                throw new ParseException("not implemented");
+
+            case DataType.BYTEARRAY:
+                fieldClass = Byte[].class;
+                break;
+
+            case DataType.CHARARRAY:
+            case DataType.BIGCHARARRAY:
+                fieldClass = String.class;
+                break;
+
+            default:
+                throw new ParseException("unknown data type");
+        }
+    }
+
+    public Object valueOf() {
+
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public Class getFieldClass() {
+        return fieldClass;
+    }
+
+    public XSchema getFieldSchema() {
+        return schema;
+    }
+
+}
