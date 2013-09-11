b6c06168ef07f164e01e58dde409f4a6e44d55ab
Pavel N. Savenkov <xeenon@me.com>
2013-09-11 14:54:11 +0400

    test

--- test/com/xeenon/pig/test/simple/Test_ParseUnixTimeMilis.java
+++ test/com/xeenon/pig/test/simple/Test_ParseUnixTimeMilis.java
@@ -29,6 +29,7 @@ public class Test_ParseUnixTimeMilis {
         ips.put("2013-09-08 00", 1378587599l);
         ips.put("2013-09-11 00", 1378843200l);
         ips.put("2013-09-16 00", 1379275200l);
+        ips.put("2013-09-19 00", 1379534400l);
     }
 
     @Test
